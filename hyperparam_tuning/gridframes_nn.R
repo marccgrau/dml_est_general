d7a7b00ba855b258ba1e51a08af256701f7f036e
marccgrau <marcgrau@outlook.com>
2020-10-28 14:54:03 +0100

    cross entropy error function for neural net predictions of propensity score

--- hyperparam_tuning/gridframes_nn.R
+++ hyperparam_tuning/gridframes_nn.R
@@ -16,3 +16,21 @@ gridframe_oc_1 = function(){
   return(grid_frame_nn)
 }
 
+gridframe_ps_1 = function(){
+  # define set of possible hyperparameters
+  params_nn = list(
+    act.fct = c("logistic"),
+    algorithm = c("rprop-"),
+    neurons = c(6:10),
+    threshold = c(0.2, 0.1),
+    err.fct = "ce",
+    stepmax = 200000,
+    linear.output = TRUE,
+    rep = c(1)
+  )
+  
+  # expand the grid according to defined hyperparameters
+  grid_frame_nn = expand.grid(params_nn)
+  return(grid_frame_nn)
+}
+
