bf6cb43aaf2750bbda5822961d405675218441ed
marccgrau <marcgrau@outlook.com>
2021-07-04 15:57:49 +0200

    update bash script

--- simulations/simulation_1_50/start_simulation_1_50.sh
+++ simulations/simulation_1_50/start_simulation_1_50.sh
@@ -6,12 +6,12 @@ chmod +x part_1_sim1_50.R
 echo part_ff_sim1_50.R
 chmod +x part_ff_sim1_50.R
 
-Rscript part_1_sim1_50.R
+Rscript ./part_1_sim1_50.R
 Write-Host "Congratulations! Part 1 of simulation 1 executed successfully. Starting with part 2."
 
 for i in {2..11..1}
 	do
-    	Rscript part_ff_sim1_50.R
+    	Rscript ./part_ff_sim1_50.R
 	
 	if [[ $i -lt 10 ]]
 	then
