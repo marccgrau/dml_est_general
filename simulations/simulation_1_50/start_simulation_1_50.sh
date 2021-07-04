93e9f5ec28b94a4572c4d2c09928ea801e2ba409
marccgrau <marcgrau@outlook.com>
2021-07-04 15:15:30 +0200

    bash file to let simulations run on linux

---
+++ simulations/simulation_1_50/start_simulation_1_50.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo part_1_sim1_50.R
+chmod +x part_1_sim1_50.R
+
+echo part_ff_sim1_50.R
+chmod +x part_ff_sim1_50.R
+
+Rscript part_1_sim1_50.R
+Write-Host "Congratulations! Part 1 of simulation 1 executed successfully. Starting with part 2."
+
+for i in {2..11..1}
+	do
+    	Rscript.exe part_ff_sim1_50.R
+	
+	if [[ $i -lt 10 ]]
+	then
+	Write-Host "Congratulations! Part $($i) of simulation 1 executed successfully. Starting with part $($i+1)."
+	else 
+	Write-Host "Congratulations! Part $($i) of simulation 1 executed successfully. The simulation ended successfully."
+	fi
+done
