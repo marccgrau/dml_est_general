f708ea52cffa21bf81e058595e5e70164722aca2
marccgrau <marcgrau@outlook.com>
2021-07-14 08:23:36 +0200

    Optimizer testing for new processes

--- hyperparam_tuning/randomsearch_xgb.R
+++ hyperparam_tuning/randomsearch_xgb.R
@@ -4,10 +4,10 @@ randomsearch_xgb_oc = function(){
   for (i in 1:500){
     param <- list(booster = "gbtree", # to account for non-linearities a tree based estimator is used
                   objective = "reg:squarederror",
-                  max_depth = sample(3:10, 1),
+                  max_depth = sample(1:10, 1),
                   eta = runif(1, 0.01, 0.4),
-                  subsample = runif(1, 0.7, 1),
-                  colsample_bytree = runif(1, 0.6, 1),
+                  subsample = runif(1, 0.6, 1),
+                  colsample_bytree = runif(1, 0.4, 1),
                   min_child_weight = sample(0:10, 1),
                   alpha = sample(0:6, 1),
                   nrounds= 300,
@@ -29,10 +29,10 @@ randomsearch_xgb_ps = function(){
   for (i in 1:500){
     param <- list(booster = "gbtree", # to account for non-linearities a tree based estimator is used
                   objective = "binary:logistic",
-                  max_depth = sample(3:10, 1),
+                  max_depth = sample(1:10, 1),
                   eta = runif(1, 0.01, 0.4),
-                  subsample = runif(1, 0.7, 1),
-                  colsample_bytree = runif(1, 0.6, 1),
+                  subsample = runif(1, 0.6, 1),
+                  colsample_bytree = runif(1, 0.4, 1),
                   min_child_weight = sample(0:10, 1),
                   alpha = sample(0:6, 1),
                   nrounds= 300,
